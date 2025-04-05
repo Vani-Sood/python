2e4f3add151da9dee709846267ad6eb7fccc0c9c
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2025-04-05 23:27:50 +0530

    Update variable.py

--- variable.py
+++ variable.py
@@ -3,3 +3,5 @@ age = 21
 percentage = 98.6
 is_student = True
 print ("my name is " , name , ", my age is ", age, ", my percentage is ", percentage, "and I am a student ", is_student)
+#expressions
+print(percentage-1.0)
