c9bcf74b4a60986f5983a4428e99d1f52d0519f1
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2025-04-05 23:06:46 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,5 @@
+name = "Vani"
+age = 21
+percentage = 98.6
+is_student = True
+print ("my name is " , name , ", my age is ", age, ", my percentage is ", percentage, "and I am a student ", is_student)
