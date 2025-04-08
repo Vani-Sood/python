a63d5ad39ce0fdcb40df35be4a5dfc552053c4dd
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2025-04-08 22:55:25 +0530

    Create biodata.py

---
+++ biodata.py
@@ -0,0 +1,15 @@
+name = "Vani"
+age = 21
+percentage = 98.6
+is_student = True
+print ("my name is " , name , ", my age is ", age, ", my percentage is ", percentage, "and I am a student ", is_student)
+#expressions
+print(percentage-1.0)
+#print with seperator it can be anything 
+print (name , age, percentage, is_student, sep="-")
+
+#we can print series like
+x=1
+y=2
+z=3
+print(x, y, z, sep="->")
