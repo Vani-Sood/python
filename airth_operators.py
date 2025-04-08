e980dc73c68e2ef1c0d30010d6fbb2fd15f1732c
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2025-04-08 22:53:02 +0530

    airth_operators.py

---
+++ airth_operators.py
@@ -0,0 +1,18 @@
+n1=int(input("first number "))
+n2=int(input("second number "))
+sum= n1+n2
+mul=n1*n2
+div=n1/n2
+sub=n1-n2
+#floor_div returns nearest whole number on division
+floor_division=n1//n2
+remainder=n1%n2
+exponent=n1**n2
+print(sum)
+print(mul)
+print(div)
+print(sub)
+print(floor_division)
+print(remainder)
+print(exponent)
+
