83bb2456e61a5731ad70190963733da58d2bc901
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2025-04-25 11:23:40 +0530

    Create change_index_in_list.py

---
+++ change_index_in_list.py
@@ -0,0 +1,5 @@
+fruits= ["apple", "mango", "cherry", "banana"]
+fruits[1]= "kiwi"  #change element at index one
+print(fruits)
+fruits[1:3]=["papaya"]   #change element at index range
+print(fruits)
