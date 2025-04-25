b753d7415995d92b1c16c440cc5236a3929cd24a
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2025-04-25 11:29:18 +0530

    Create remove_in_list.py

---
+++ remove_in_list.py
@@ -0,0 +1,5 @@
+fruits= ["apple", "mango", "cherry", "banana"]
+fruits.remove("banana") #remove banana
+print(fruits)
+fruits.pop(1)  #remove from index 1, if we wont write anything in index it will automatically select last
+print(fruits)
