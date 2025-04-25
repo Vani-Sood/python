f9a0244a8a6993d808686e1d60bd584b920dfd38
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2025-04-25 11:15:34 +0530

    Create div_by5&3_but!15.py

---
+++ div_by5&3_but!15.py
@@ -0,0 +1,9 @@
+a=int(input("enter a no. "))
+if a%15==0:
+    print("Invalid no. ")
+else:
+     if a%3== 0 or a%5== 0:
+          print ("no. is divisible by 3 or 15")
+     else:
+          print("number is neither the case")
+        
