07a505e1341e51bf4cf43c4415ecbbd748039215
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2025-04-25 11:22:47 +0530

    Create add_element_in_list.py

---
+++ add_element_in_list.py
@@ -0,0 +1,9 @@
+fruits= ["apple", "mango", "cherry", "banana"]
+print(fruits)
+fruits.append("kiwi") #adds element to end of list
+print(fruits)
+fruits.insert(2, "kiwi") #adds element to index 2 of list
+print(fruits)
+veg= ["potato","pumpkin"]
+fruits.extend(veg)  #combines 2 list
+print(fruits)
