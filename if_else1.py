185a04f746c7d21966f4e236b5e1cb61b94d9598
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2025-04-25 11:19:46 +0530

    Create if_else1.py

---
+++ if_else1.py
@@ -0,0 +1,13 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if a>b:
+    if a>c:
+        print(a, " is greatest")
+    else:
+        print(c, " is graetest")
+else: 
+    if b>c:
+        print(b, " is greatest")
+    else:
+        print(c, " is greatest")    
