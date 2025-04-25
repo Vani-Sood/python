7abece0c400c728eed51d4238d73b8b4340bf369
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2025-04-25 11:24:33 +0530

    Create list_in_for_loop.py

---
+++ list_in_for_loop.py
@@ -0,0 +1,15 @@
+list= [10,20,30,40,50]
+fruit=["apple", "banana", "kiwi"]
+for i in list:
+    print(i)
+#for i in fruit:
+#    print(i)
+print(len(list)) #check length of list
+if 10 in list:
+    print("10 is there")
+if 33 not in list:
+    print("33 is not in list")    
+#print(list[2])#will print 30
+#print(list[-3])#negative indexing starting from last at -1
+#print(list[1:3])#will give range from 1 to 3 excluding 3
+#print(list[-4:-2])#negative index range
