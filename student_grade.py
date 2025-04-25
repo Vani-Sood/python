2f725d5eb7ecacd92706a7fb6c28bf76da1f0642
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2025-04-25 10:54:18 +0530

    Add files via upload

---
+++ student_grade.py
@@ -0,0 +1,11 @@
+marks= int(input("enter the marks percentage "))
+if marks>=81:
+    print("very good")
+elif marks>=61:
+    print("good")    
+elif marks>=41:
+    print("average")
+elif marks<=40:
+    print("fail")   
+else:
+    print("invalid marks")         
